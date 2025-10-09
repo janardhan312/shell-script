58917e58503934d50443977b975cf11dead228b9
Janardhan <janardhancloud312@gmailcom>
2025-10-09 20:47:59 +0530

    modify

--- 2-shellby-mustang.sh
+++ 2-shellby-mustang.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh:: Hi sureh"
-echo "Suresh:: Hi ramesh"
-echo "Ramesh:: How was the day"
-echo "Suresh:: Its amazing ramesh"
+PERSON1=JOHN
+PERSON2=NIKI
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hi $PERSON1"
+echo "$PERSON1:: How was the day"
+echo "$PERSON2:: Its amazing $PERSON1"
