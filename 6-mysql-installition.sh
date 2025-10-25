1c44617857bd7977f5005440f16baa1d4e7f6c25
Janardhan <janardhancloud312@gmailcom>
2025-10-25 21:44:27 +0530

    update

--- 6-mysql-installition.sh
+++ 6-mysql-installition.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then 
     echo "ERROR:: kindly run this script with root access"
+    exit 1
 
 fi
     
@@ -11,6 +12,7 @@ dnf install mysql -y
 
 if [ $? -ne 0 ]; then 
     echo "ERROR:: Installtion failure"
+    exit 1
 else 
     echo "Installtion success"
 fi    
